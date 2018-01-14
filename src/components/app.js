05cd8a5b6e7c5470e1409534394b062c152e0806
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-14 12:27:51 -0800

    Condense export statement in root reducer as per es6; pseudocode basic app component rendering simple div

---
+++ src/components/app.js
@@ -0,0 +1,6 @@
+// import React and Component from react
+
+// export default class App extends Component
+	// render
+		// return	
+			// div with simple text
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,11 @@
 // import combineReducers from redux
 
-// create const rootReducer set to call to combineReducers 
+// export default const rootReducer set to call to combineReducers 
 	// dummy state set to (state = {} )=> state
-
-// export default rootReducer
 
 import { combineReducers } from 'redux';
 
-const rootReducer = combineReducers({
+export default const rootReducer = combineReducers({
   state: (state = {}) => state
 });
 
-export default rootReducer;
