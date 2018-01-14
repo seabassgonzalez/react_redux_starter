7e0cb18ebfb8cadc0f94dab04f7705cd5a2b5323
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-14 12:17:49 -0800

    Implement root index connecting redux store with middleware and reducers

--- src/index.js
+++ src/index.js
@@ -12,3 +12,22 @@
 	// provider component with store property set to object callback to createStoreWithMiddleware passing reducers
 		// app component
 	// second argument to .render being location to render to
+
+	
+	
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
+	
