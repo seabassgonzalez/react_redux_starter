e405fd601c620246ecfad6250d5d340cab4aef81
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-14 12:13:33 -0800

    Pseudocode root index.js rendering app component, creating store with middleware connecting reducers

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// import React from react
+// import ReactDOM from react-dom
+// import Provider from react-redux
+// import createStore and applyMimddleware from redux
+
+// import App from components/app
+// import reducers from reducers
+
+// create const createStoreWithMiddleware set to applyMiddleware() call passing in createStore
+
+// ReactDOM.render
+	// provider component with store property set to object callback to createStoreWithMiddleware passing reducers
+		// app component
+	// second argument to .render being location to render to
