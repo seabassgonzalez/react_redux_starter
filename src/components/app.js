f31a5c8f9a51f47050b544834a443e1a8a13fd84
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-14 12:29:51 -0800

    Implement simple app component returning text div

--- src/components/app.js
+++ src/components/app.js
@@ -4,3 +4,13 @@
 	// render
 		// return	
 			// div with simple text
+
+import React, { Component } from 'react';
+
+export default class App extends Component {
+	render(){
+		return (
+			<div>Simple text placeholder</div>
+		);
+	}
+}
